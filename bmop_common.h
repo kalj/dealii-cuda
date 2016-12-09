86822a6fa0620c3695f2fb2ef171cb2713f1e943
Karl Ljungkvist <k.ljungkvist@gmail.com>
2016-12-09 10:52:48 +0100

    new non-uniform 2D meshes for benchmarks, with higher ratio of hanging nodes

--- bmop_common.h
+++ bmop_common.h
@@ -8,9 +8,9 @@
 
 template <int dim>
 void mark_cells_in_annulus(dealii::Triangulation<dim> &triangulation,
-                           const dealii::Point<dim> center,
-                           double r,
-                           double R)
+                           double R,
+                           double r=0.0,
+                           const dealii::Point<dim> center=dealii::Point<dim>())
 {
   for (typename
          dealii::Triangulation<dim>::active_cell_iterator
@@ -23,51 +23,85 @@ void mark_cells_in_annulus(dealii::Triangulation<dim> &triangulation,
   }
 }
 
+
+template <int dim>
+void mark_cells_on_shell(dealii::Triangulation<dim> &triangulation,
+                         double R,
+                         const dealii::Point<dim> center=dealii::Point<dim>())
+{
+  const int nverts = 1<<dim;
+
+  for (typename
+         dealii::Triangulation<dim>::active_cell_iterator
+         cell=triangulation.begin_active(); cell != triangulation.end(); ++cell) {
+    if (cell->is_locally_owned()) {
+      int ninside = 0;
+
+      for(int i = 0; i < nverts; ++i)
+        ninside += (cell->vertex(i).distance(center) < R);
+
+      if(ninside!=0 && ninside !=nverts)
+        cell->set_refine_flag();
+    }
+  }
+}
+
 template <int dim>
 void pseudo_adaptive_refinement(dealii::Triangulation<dim> &triangulation,
                                 domain_case_t domain,
                                 int n_ref)
 {
-  triangulation.refine_global (std::max(n_ref-2,0));
+
+  n_ref = std::max(n_ref-2,0);
+
+  triangulation.refine_global (n_ref);
 
   float reduction = 0;
-  if(domain == BALL) {
-    if(dim==2)
-      reduction = 0.021;
+  if(dim==2) {
+    if(domain == BALL)
+      reduction = 0.025;
     else
-      reduction = 0.04;
+      reduction = 0.005;
   }
-  else if(dim==3) {
-    reduction = 0.015;
+  else {
+    if(domain == BALL)
+      reduction = 0.04;
+    else
+      reduction = 0.015;
   }
 
   // the radii in this refinement are adjusted such that we hit similar number
   // of dofs as the globally refined case on a similar situation
 
   // refine elements inside disk of radius 0.55
-  mark_cells_in_annulus(triangulation,dealii::Point<dim>(),0.0,0.55-reduction);
+  mark_cells_in_annulus(triangulation,0.55-reduction);
   triangulation.execute_coarsening_and_refinement();
 
   // refine elements in annulus with radius 0.3 < r < 0.42
-  mark_cells_in_annulus(triangulation,dealii::Point<dim>(),0.3+reduction,0.42-reduction);
+  mark_cells_in_annulus(triangulation,0.42-reduction,0.3+reduction);
   triangulation.execute_coarsening_and_refinement();
 
   // refine elements in annulus with radius 0.335 < r < 0.41 (or 0.388)
-  mark_cells_in_annulus(triangulation,dealii::Point<dim>(),0.32+reduction,0.41-reduction);
+  mark_cells_in_annulus(triangulation,0.41-reduction,0.32+reduction);
   triangulation.execute_coarsening_and_refinement();
 
-  {
-    dealii::Point<dim> offset;
-    for (unsigned int d=0; d<dim; ++d)
-      offset[d] = -0.1*(d+1);
+  dealii::Point<dim> offset;
+  for (unsigned int d=0; d<dim; ++d)
+    offset[d] = -0.1*(d+1);
 
-    mark_cells_in_annulus(triangulation,offset,0.17+reduction,0.33-reduction);
-    triangulation.execute_coarsening_and_refinement();
+  mark_cells_in_annulus(triangulation,0.33-reduction,0.17+reduction,offset);
+  triangulation.execute_coarsening_and_refinement();
 
-    mark_cells_in_annulus(triangulation,offset,0.21+reduction,0.31-reduction);
-    triangulation.execute_coarsening_and_refinement();
+  mark_cells_in_annulus(triangulation,0.31-reduction,0.21+reduction,offset);
+  triangulation.execute_coarsening_and_refinement();
+
+  if(dim==2) {
+    int nshells=4;
+    for(int s = 0; s < nshells; ++s) {
+      mark_cells_on_shell(triangulation,0.25,offset);
+      triangulation.execute_coarsening_and_refinement();
+    }
   }
-
 }
 
 
