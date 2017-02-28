9bc80bee97af02525b737e7298bfa5ac7a0b972b
Karl Ljungkvist <k.ljungkvist@gmail.com>
2017-02-28 18:27:45 +0100

    add script for building externally accessible directories

---
+++ scripts/setup_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# @(#)setup_build.sh
+# @author Karl Ljungkvist <k.ljungkvist@gmail.com>
+
+ADDITIONAL_FLAGS=$@
+
+mkdir build_debug
+cd build_debug
+cmake  -D CMAKE_CXX_FLAGS="--std=c++11 -march=native ${ADDITIONAL_FLAGS}" -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+make -j matrix_free_gpu_lib || exit 1
+cd ..
+
+
+mkdir build_release
+cd build_release
+cmake  -D CMAKE_CXX_FLAGS="--std=c++11 -march=native ${ADDITIONAL_FLAGS}" -DCMAKE_BUILD_TYPE=Release .. || exit 1
+make -j matrix_free_gpu_lib  || exit 1
+cd ..
