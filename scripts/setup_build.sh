c8390de574e4d9f11f34316a52bd25e03367d3a1
Karl Ljungkvist <k.ljungkvist@gmail.com>
2017-02-28 18:32:13 +0100

    also copy headers into build directories

--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -10,6 +10,7 @@ cd build_debug
 cmake  -D CMAKE_CXX_FLAGS="--std=c++11 -march=native ${ADDITIONAL_FLAGS}" -DCMAKE_BUILD_TYPE=Debug .. || exit 1
 make -j matrix_free_gpu_lib || exit 1
 cd ..
+cp -r matrix_free_gpu  build_debug
 
 
 mkdir build_release
@@ -17,3 +18,4 @@ cd build_release
 cmake  -D CMAKE_CXX_FLAGS="--std=c++11 -march=native ${ADDITIONAL_FLAGS}" -DCMAKE_BUILD_TYPE=Release .. || exit 1
 make -j matrix_free_gpu_lib  || exit 1
 cd ..
+cp -r matrix_free_gpu  build_release
